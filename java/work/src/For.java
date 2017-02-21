efc124086503a2b37177da80e4f2610a8cfdfbc6
Cindy <759501948@qq.com>
2017-02-21 12:05:13 +0800

    for while

---
+++ java/work/src/For.java
@@ -0,0 +1,11 @@
+public class for
+{
+	public static void main(String args []){
+		/*System.out.println("Do for");*/
+		for (int i = 0 ; i < 10 ; i = i + 2)
+		{
+			System.out.println(i);
+		}
+		System.out.println("Ending");
+	}
+}
