e2474cd693f7ff083e5746ba2fc8752562dccf9f
Cindy <759501948@qq.com>
2017-02-24 10:28:40 +0800

    Override

---
+++ java/work/src/override/Test.java
@@ -0,0 +1,17 @@
+class Test 
+{
+	public static void main(String[] args) 
+	{
+		Student s = new Student();
+		s.name = "Cindy";
+		s.age = 12;
+		s.address = "zhongshan";
+		s.introduce();
+		
+		Person p = new Person();
+		p.name = "Candy";
+		p.age = 13;
+		p.introduce();
+
+	}
+}
